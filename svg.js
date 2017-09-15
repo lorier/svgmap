5d62ed2c63021af28fc25fe9f8113fdcaf61697f
Lorie Ransom <lorie@ransomdesign.com>
2017-09-14 19:43:37 -0700

    added zoom for all gardens

--- svg.js
+++ svg.js
@@ -9,10 +9,14 @@ window.onload = function(){
 	//here it doesn't make much difference, until we combine later
 	
 	var bg = atg.select("#bg-layer");
+
+
 	var bgX = bg.getBBox().cx;
 	var bgY = bg.getBBox().cy;
+	var bgW = bg.getBBox().width;
+	var bgH = bg.getBBox().height;
 
-	var paths = atg.selectAll("path"); //get all paths
+	var paths = atg.selectAll(".garden"); //get all gardens
 
 	var mapx;
 	var mapy;
@@ -34,24 +38,68 @@ window.onload = function(){
 
 		clickedPath = obj.attr("id");
 		console.log(clickedPath + " center: " + cx + ", " + cy);
+		console.log("bg center: " + mapx + ", " + mapy);
+
 		return { "cx" : mapx, "cy" : mapy }
 
 	}
 
+	function getPathSize(obj){
+		var w = obj.getBBox().width
+		var h = obj.getBBox().height;
+
+		return {
+			w: w,
+			h: h
+		}
+	}
+	function calculateZoom(obj){
+		var pct;
+		var m = getPathSize(obj);
+		
+		//if the width to height ratio of the garden is greater than that of the map
+		var bgRatio = bgW/bgH;
+		var gRatio = obj.getBBox().width/obj.getBBox().height;
+
+		if (gRatio <= bgRatio){
+			pct = bgH/m.h;
+			console.log("scale percentage based on height:" + pct);
+			return pct;
+		}else {
+			pct = bgW/m.w;
+			console.log("scale percentage based on width:" + pct);
+			return pct;
+		}
+		return null;
+	}
+
+	//Click Handlers
+
 	function zoomMap(evt){
 
 		pt = getScalingCenter(this);
-		console.log(this);
-		
-		console.log(pt);
 
-		x.toggleClass("inactive");
-		// bg.animate({transform: "s2.1 2.1" + cx + " " + cy}, 500, mina.bounce);
-		bg.animate({transform: "T"+pt.cx+" "+pt.cy+"S2 2 728 470"}, 200, mina.easein); //test based on creek center
-		enableClickhandlers(false);
+		pct = calculateZoom(this);
+
+		// console.log(this);
+		
+		// console.log(pt);
+
+		x.removeClass("inactive");
+
+		bg.animate({transform: "T"+pt.cx+" "+pt.cy+"S"+pct*.9 + " " + pct*.9 + " " + bgW/2 + " " + bgH/2}, 200, mina.easein); 
+		
+		console.log("bg dimensions: w=> " + bg.getBBox().width + " h=>" + bg.getBBox().height);
+		console.log("Garden dimensions: w=>" + this.getBBox().width + " h=>" + this.getBBox().height);
+		console.log(" ");
+		// enableClickhandlers(false);
 	}
 
-
+	function zoomMapOut(evt){
+		x.addClass("inactive");
+		bg.stop().animate({transform:"s1 1"}, 200, mina.easeout);
+		// enableClickhandlers(true);
+	}
 
 	function makeClose(){
 		x = atg.text(50, 50, "X");
@@ -61,12 +109,6 @@ window.onload = function(){
 			id: "close"
 		})
 		x.click(zoomMapOut);
-	}
-
-	function zoomMapOut(evt){
-		this.toggleClass("inactive");
-		bg.stop().animate({transform:"s1 1"}, 200, mina.easeout);
-		enableClickhandlers(true);
 	}
 
 	function enableClickhandlers(bool){
